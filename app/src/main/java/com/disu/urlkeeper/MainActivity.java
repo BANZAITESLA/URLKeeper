cab6e9c5c9e9c34b3f09b0a8335f511bed67fcf7
Dea Inesia Sri Utami <deainesia223@gmail.com>
2022-07-31 12:33:47 +0700

    toolbar layout created

--- app/src/main/java/com/disu/urlkeeper/MainActivity.java
+++ app/src/main/java/com/disu/urlkeeper/MainActivity.java
@@ -8,59 +8,61 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 
+import com.disu.urlkeeper.fragment.UrlManagerFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //      set default fragment
-//        loadFragment(new HomeFragment());
+        loadFragment(new UrlManagerFragment());
 //        loadFragment(new ProfileFragment());
 //        loadFragment(new GalleryFragment());
-//        BottomNavigationView bottom_nav = findViewById(R.id.bottomNavigationView);
-//        bottom_nav.setOnItemSelectedListener(this);
-//        bottom_nav.setSelectedItemId(R.id.menu_home);
+        BottomNavigationView bottom_nav = findViewById(R.id.bottomNavigationView);
+        bottom_nav.setOnItemSelectedListener(this);
+        bottom_nav.setSelectedItemId(R.id.menu_url);
     }
 
-//    private boolean loadFragment(Fragment fragment){
-//        if (fragment != null) {
-//            getSupportFragmentManager().beginTransaction()
-////                    .setCustomAnimations(
-////                            R.anim.fade_in,  // enter
-////                            R.anim.fade_out,  // exit
-////                            R.anim.fade_in,   // popEnter
-////                            R.anim.fade_out  // popExit
-////                    )
-//                    .replace(R.id.frame_layout, fragment)
-//                    .addToBackStack(null)
-//                    .commit();
-//            return true;
-//        }
-//
-//        return false;
-//    }
+    private boolean loadFragment(Fragment fragment){            // load fragment setter
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+//                    .setCustomAnimations(
+//                            R.anim.fade_in,  // enter
+//                            R.anim.fade_out,  // exit
+//                            R.anim.fade_in,   // popEnter
+//                            R.anim.fade_out  // popExit
+//                    )
+                    .replace(R.id.frame_layout, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
 
-//    @Override
-//    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//        Fragment fragment = null;
-//        switch (item.getItemId()){
+        return false;
+    }
+
+    @Override           // navigation setter
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()){
 //            case R.id.menu_home:
-////                fragment = new HomeFragment();
+////                fragment = new UrlManagerFragment();
 //                break;
 //            case R.id.menu_tag:
-////                fragment = new DailyActivityFragment();
+////                fragment = new UrlManagerFragment();
 //                break;
-//            case R.id.menu_url:
-////                fragment = new MusicVideoFragment();
-//                break;
-//        }
-//        return loadFragment(fragment);
-//    }
+            case R.id.menu_url:
+                fragment = new UrlManagerFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+
 
     @Override
     public void onBackPressed() {
